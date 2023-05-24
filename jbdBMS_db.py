5a32f7a82b7e4dc89e11d6e5e69fda2fd64a5a43
michaelpappas <pappas.michael@gmail.com>
2023-05-24 08:43:22 -0700

    code cleanup

--- jbdBMS_db.py
+++ jbdBMS_db.py
@@ -1,48 +1,22 @@
-#!/usr/bin/env python3
 # This is the 2 temperature sensor version of the code.
 #
 # This code should be run as SUDO as it does low level BLE scanning.
 #
 # Run it as follows:
 #
-#   sudo python jbd-bms-mqtt.py -n "Cabin 580ah" -i 0 -t solar
-#   or
-#   sudo python jbd-bms-mqtt.py -a "a4:c1:38:30:c2:8e" -i 0 -t solar
-#
-# This will scan for a bluetooth device called "Cabin 580ah" and extract its
-# bluetooth address, or, use the passed address if provided, then try and attach
-# to the BMS and read a set if variables, one time, and then publish a MQTT
-# message with those variables to the Topic "solar".
-#
-# USAGE NOTE
-# If you use a value other than 0 for the -i parm the program will loop forever
-# and read the BMS values every X minutes based on the value you passed. This is
-# not recommended as a long term method to run the script, as it could cause
-# resource issues in the python environment. For an ongoing run please use a
-# cron job to control run times and pass a 0 for the -i argument...
-#
-# In addition, if you run this program via a manual shell loop instead of using
-# cron, and someone attached to the BMS using a bluetooth app (like XiaoxiangBMS
-# or SmartBMSUtility) the code will fail when it attempts to connect (as the other
-# app has the connection) and will throw an exception. If you started it intending
-# it to run "forever" in a terminal window you will not realize it's failed.
-#
-# If cron is used, at least the process will be kicked off again based on the cron
-# process and try to reconnect again, automatically...
-#
-# You've been warned!
-#
+#   sudo python jbd-bms-mqtt.py -n "xiaoxiang BMS" -i 0 -t solar
+
 
 from bluepy.btle import Peripheral, DefaultDelegate, BTLEException
 from bluepy.btle import Scanner, DefaultDelegate
 import sys
 import struct
 import argparse
-import json
+# import json
 import time
 import binascii
 import atexit
-import paho.mqtt.client as paho
+# import paho.mqtt.client as paho
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Battery, db_url
@@ -141,8 +115,8 @@ bleName = args.name  # BLE device Name that will be scanned for to get address
 bleAddrP = args.address  # BLE address that will be used rather than a BLE name
 timeSleep = 1  # Used to slow things down a bit in case of timing issues
 ginfo = list()  # Global list to hold elements from the various requests
-broker = "127.0.0.1"  # Change this to your MQTT broker address (MP set to localhost)
-port = 1883  # Default port that MQTT is listening on
+# broker = "127.0.0.1"  # Change this to your MQTT broker address (MP set to localhost)
+# port = 1883  # Default port that MQTT is listening on
 
 # Resolve the BLE device address using a name or a provided address
 if not bleAddrP and not bleName:
